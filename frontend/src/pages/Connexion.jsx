43bde71ca5c99be94a84a80347c20b3f20a925f3
traoremodibobintou-source <traoremodibobintou@gmail.com>
2026-09-21 01:15:32 +0000

    Update API endpoint for login request

--- frontend/src/pages/Connexion.jsx
+++ frontend/src/pages/Connexion.jsx
@@ -16,7 +16,7 @@ function Connexion() {
     setChargement(true);
 
     try {
-      const response = await fetch("http://127.0.0.1:8001/api/login", {
+     const response = await fetch("https://vignette-controleur.onrender.com/api/login", { 
         method: "POST",
         headers: {
           "Content-Type": "application/json",
