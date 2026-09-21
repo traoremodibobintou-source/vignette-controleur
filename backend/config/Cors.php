1b0920c0effba88fa7f41d3e453bee3aaf5d05f4
traoremodibobintou-source <traoremodibobintou@gmail.com>
2026-09-21 01:41:36 +0000

    Add CORS configuration in Cors.php

---
+++ backend/config/Cors.php
@@ -0,0 +1,23 @@
+<?php
+
+return [
+
+    'paths' => ['api/*'],
+
+    'allowed_methods' => ['*'],
+
+    'allowed_origins' => [
+        'https://traoremodibobintou-source.github.io',
+    ],
+
+    'allowed_origins_patterns' => [],
+
+    'allowed_headers' => ['*'],
+
+    'exposed_headers' => [],
+
+    'max_age' => 0,
+
+    'supports_credentials' => false,
+
+];
