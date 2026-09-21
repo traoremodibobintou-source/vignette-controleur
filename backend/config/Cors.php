24fb3316d79852baac9324a039dc71ea00edd603
traoremodibobintou-source <traoremodibobintou@gmail.com>
2026-09-21 02:02:15 +0000

    Refine CORS configuration with specific methods and headers

--- backend/config/Cors.php
+++ backend/config/Cors.php
@@ -4,7 +4,14 @@ return [
 
     'paths' => ['api/*'],
 
-    'allowed_methods' => ['*'],
+    'allowed_methods' => [
+        'GET',
+        'POST',
+        'PUT',
+        'PATCH',
+        'DELETE',
+        'OPTIONS',
+    ],
 
     'allowed_origins' => [
         'https://traoremodibobintou-source.github.io',
@@ -12,7 +19,12 @@ return [
 
     'allowed_origins_patterns' => [],
 
-    'allowed_headers' => ['*'],
+    'allowed_headers' => [
+        'Content-Type',
+        'Accept',
+        'Origin',
+        'Authorization',
+    ],
 
     'exposed_headers' => [],
 
